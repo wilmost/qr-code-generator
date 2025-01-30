83ee54a3b7b94dd1eb24cd582a46b636b1d09537
wilmost <wilmomartinezs@gmail.com>
2025-01-29 22:21:10 -0400

    added addons eks tf file

---
+++ terraform/addons.tf
@@ -0,0 +1,23 @@
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name  = aws_eks_cluster.eks.name
+  addon_name    = "vpc-cni"
+  addon_version = "latest"
+}
+
+resource "aws_eks_addon" "coredns" {
+  cluster_name  = aws_eks_cluster.eks.name
+  addon_name    = "coredns"
+  addon_version = "latest"
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name  = aws_eks_cluster.eks.name
+  addon_name    = "kube-proxy"
+  addon_version = "latest"
+}
+
+resource "aws_eks_addon" "aws_ebs_csi" {
+  cluster_name  = aws_eks_cluster.eks.name
+  addon_name    = "aws-ebs-csi-driver"
+  addon_version = "latest"
+}
