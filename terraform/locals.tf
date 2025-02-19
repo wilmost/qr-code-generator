64dbd8192bb665feae5a1029afbf4ec19f292d44
wilmost <wilmomartinezs@gmail.com>
2025-02-18 22:26:09 -0400

    Udatating locals.tf

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,10 +1,12 @@
+
+
 locals {
-  region           = "us-west-2"
+  region           = "us-east-1"
   cluster_name    = "alpha-cluster-eks"
   node_group_name = "alpha-cluster-ng"
   instance_type   = "t3.medium"
   desired_size    = 2
   max_size        = 3
   min_size        = 1
-  subnet_ids      = ["subnet-a", "subnet-b"]
+  subnet_ids      = ["subnet-0322734cd84d8a529","subnet-02caa4d274cf32d2b"]
 }
