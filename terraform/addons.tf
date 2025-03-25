111d58d63f9dd2f769e9a339893c2932bdb14fe1
wilmost <wilmomartinezs@gmail.com>
2025-03-24 21:34:09 -0400

    update addon conifguration

--- terraform/addons.tf
+++ terraform/addons.tf
@@ -1,13 +1,13 @@
 resource "aws_eks_addon" "vpc_cni" {
   cluster_name  = aws_eks_cluster.eks.name
   addon_name    = data.aws_eks_addon_version.vpc_cni.addon_name
-  addon_version = data.aws_eks_addon_version.vpc_cni.version
+  addon_version = data.aws_eks_addon_version.vpc_cni.version 
 }
 
 resource "aws_eks_addon" "coredns" {
   cluster_name  = aws_eks_cluster.eks.name
   addon_name    = data.aws_eks_addon_version.coredns.addon_name
-  addon_version = data.aws_eks_addon_version.coredns.version
+  addon_version = data.aws_eks_addon_version.coredns.version 
 }
 
 resource "aws_eks_addon" "kube_proxy" {
