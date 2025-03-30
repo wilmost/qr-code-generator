b90b9920004b7bcc48a168623e7070cdb072e323
wilmost <wilmomartinezs@gmail.com>
2025-03-30 12:43:11 -0400

    updated nextconfig fie

--- front-end-nextjs/next.config.js
+++ front-end-nextjs/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: `${process.env.APP_BACKEND_URL}/:path*`,
+			},
+		];
+	},
+
+}
 
 module.exports = nextConfig
